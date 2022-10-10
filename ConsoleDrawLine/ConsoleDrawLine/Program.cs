739db3f6f377d3f391c572f191cad57d54ae57d8
Ksenia <ksuha280502@gmail.com>
2022-10-10 20:53:29 +0300

    Линия * с повторением (do while)

---
+++ ConsoleDrawLine/ConsoleDrawLine/Program.cs
@@ -0,0 +1,21 @@
+﻿
+bool isContinue = false;
+
+do
+{
+    Console.WriteLine("Длинна?");
+
+    //(1)
+    //int width;
+    //int.TryParse(Console.ReadLine(), out width);
+
+    
+    //(2)
+    int.TryParse(Console.ReadLine(), out int width);
+
+    Console.WriteLine(new String('*', width));
+
+
+    Console.WriteLine("Повторить[Y/N]?");
+    isContinue = (Console.ReadLine()?.ToUpper() == "Y");    
+}while (isContinue);
