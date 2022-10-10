cb26ffc4f1ecce583884826badc8320ee09cd0d6
Ksenia <ksuha280502@gmail.com>
2022-10-10 23:07:46 +0300

    Работа со списком

---
+++ ConsoleList/ConsoleList/Program.cs
@@ -0,0 +1,25 @@
+﻿
+//Список (list) 
+
+//(A) обычный список (устаревший вариант)
+
+using System.Collections;
+
+ArrayList arrayList = new();
+arrayList.Add(123);
+arrayList.Add("Hello");
+arrayList.Add(3.14);
+//...
+
+//(B) Обобщенный список (с приведением к конуретному типу)
+List<string> cities = new() {"Москва", "Воронеж", "Севастопль"};
+cities.Add("Pari");
+cities.AddRange(new string[] { "Тула", "Архангельск" });
+cities.RemoveAt(0);
+cities.Reverse();
+
+Console.WriteLine( string.Join(' ', cities));
+
+cities.Sort(); //сортировка по алфавиту
+
+Console.WriteLine( string.Join(' ', cities));
