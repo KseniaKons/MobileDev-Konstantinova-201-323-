475877f7214f566baff4596be8349451abb90e0b
Ksenia <ksuha280502@gmail.com>
2022-10-10 23:22:09 +0300

    Словарь (ключ+значение)

---
+++ ConsoleDictionary/ConsoleDictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿
+//Словарь (Dictionary)
+//ключ(id - уникальное значение) + значение 
+
+Dictionary<int, string> x = new();
+x.Add(10, "Калуга");
+x.Add(55, "Псков");
+x.Add(92, "Самара");
+x.Add(13, "Брянск");
+
+Console.WriteLine(x[92]);
+x[55] = "Воронеж";
+x[17] = "Курск";
+
+foreach(var item in x)
+{
+    Console.WriteLine($"key = {item.Key}, value = {item.Value} ");
+}
