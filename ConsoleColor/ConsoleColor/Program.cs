b7cda9e3f0158079847571cf770e0e4b83460048
Ksenia <ksuha280502@gmail.com>
2022-10-10 23:27:03 +0300

    ForegroundColor и BackgroundColor в консоли

---
+++ ConsoleColor/ConsoleColor/Program.cs
@@ -0,0 +1,13 @@
+﻿
+//
+Console.ForegroundColor = ConsoleColor.Red;
+Console.BackgroundColor = ConsoleColor.White;
+Console.WriteLine("Красный текст на белом фоне");
+Console.ResetColor();
+Console.WriteLine("Обычный текст");
+
+foreach(var color in Enum.GetValues<ConsoleColor>())
+{
+    Console.WriteLine(color);
+
+}
