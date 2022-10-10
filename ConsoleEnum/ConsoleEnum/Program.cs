c2d27bf51e2220a1c14f980e829bb5b98d1dd15c
Ksenia <ksuha280502@gmail.com>
2022-10-10 23:24:34 +0300

    Перечислимый тип (Enum)

---
+++ ConsoleEnum/ConsoleEnum/Program.cs
@@ -0,0 +1,22 @@
+﻿
+//Перечислимый тип (Enum)
+
+class Program
+{
+    public enum SingleColor: byte {Red, Green=100, Blue}
+
+    static void Main()
+    {
+        SingleColor singleColor;
+        singleColor = SingleColor.Green;
+
+        Console.WriteLine(singleColor);
+        Console.WriteLine(singleColor.ToString("D"));
+        Console.WriteLine(Enum.GetUnderlyingType(singleColor.GetType()));
+    }
+
+}
+
+
+
+
