62ae28e17b99fb68ac09618df08e15dcb2776a28
Ksenia <ksuha280502@gmail.com>
2022-10-13 09:34:51 +0300

    Создание расширения класса

---
+++ WinForMetodExt/WinForMetodExt/Form1.cs
@@ -0,0 +1,12 @@
+namespace WinForMetodExt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBox1.TextChanged += (s, e) => label1.Text = textBox1.TextExt();
+        }
+    }
+}
---
+++ WinForMetodExt/WinForMetodExt/TextBoxExt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinForMetodExt
+{
+    public static class TextBoxExt
+    {
+        public static string TextExt(this TextBox s)
+        {
+            return s.Text.ToUpper() + "!!!";
+        }
+
+    }
+}
