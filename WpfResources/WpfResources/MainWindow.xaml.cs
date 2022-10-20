978e2451908540915b93874acf1831da590f300c
Ksenia <ksuha280502@gmail.com>
2022-10-20 13:46:52 +0300

    Ресурсы (динамические, статические)

---
+++ WpfResources/WpfResources/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfResources
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        Random rnd = new Random();   
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButChangt_Background_Plai(object sender, RoutedEventArgs e)
+        {
+            this.Resources["Background_Plai"] = new SolidColorBrush(
+                Color.FromRgb((byte)rnd.Next(256), (byte)rnd.Next(256), (byte)rnd.Next(256)));
+
+        }
+
+        private void ButChangtContentPlai(object sender, RoutedEventArgs e)
+        {
+            this.Resources["Content_Plai"] = $"plai {rnd.Next(100, 1000)}";
+        }
+    }
+}
