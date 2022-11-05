60a229c1e09e313cf26295f1b936a1eac5854a96
Ksenia <ksuha280502@gmail.com>
2022-11-05 18:38:08 +0300

    Шахматная доска

---
+++ ConsoleDrawChessboard/ConsoleDrawChessboard/Program.cs
@@ -0,0 +1,32 @@
+﻿ var leters = "   a b c d e f g h   ";
+
+    Console.WriteLine(leters);
+//Console.BackgroundColor = ConsoleColor.White; 15
+//Console.BackgroundColor = ConsoleColor.Black; 0
+
+int g = 2;    
+
+    for (int x = 1; x <= 8; x++)
+    {
+        Console.Write($" {x} ");
+
+        for (int y = 0; y < 8; y++)
+        {
+            if (g%2==0)
+                Console.BackgroundColor = ConsoleColor.Black;
+            else
+                Console.BackgroundColor = ConsoleColor.White;
+
+            g++;
+
+            Console.Write("  ");
+            Console.ResetColor();
+    }
+        Console.Write($" {x} ");
+
+        g++;
+
+        Console.WriteLine();
+    }
+Console.ResetColor();
+Console.WriteLine(leters);
