ec0134041dd8880d3114e2e1b2bbd0ff54998ac0
Ksenia <ksuha280502@gmail.com>
2022-11-05 18:01:23 +0300

    Текст всеми разными цветами и фонами

---
+++ ConsoleColorText/ConsoleColorText/Program.cs
@@ -0,0 +1,17 @@
+﻿
+var colors = Enum.GetValues<ConsoleColor>();
+
+foreach (var ColorText in colors)
+{
+    Console.ForegroundColor = ColorText;
+
+    foreach (var backColor in colors)
+    {
+        Console.BackgroundColor = backColor;
+
+        Console.Write("123");
+    }
+
+    Console.WriteLine();
+}
+Console.ResetColor();
