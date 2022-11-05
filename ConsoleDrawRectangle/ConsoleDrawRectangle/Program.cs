f2a323f52d944b6e5e87157f7e8ef36a904f8196
Ksenia <ksuha280502@gmail.com>
2022-11-05 17:48:41 +0300

    Нарсовать прямоугольник

---
+++ ConsoleDrawRectangle/ConsoleDrawRectangle/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+bool isContinue = true;
+do
+{
+    Console.WriteLine("Введите длину");
+    int.TryParse(Console.ReadLine(), out int x);
+
+    Console.WriteLine("Введите ширину");
+    int.TryParse(Console.ReadLine(), out int y);
+
+    for (int i = 0; i < y; i++)
+    {
+            if (i % (y - 1) == 0)
+            {
+                Console.WriteLine(new String('*', x));
+            }
+            else
+            {
+                Console.WriteLine('*' + new String(' ', x - 2) + '*');
+            }
+    }
+
+    Console.Write("Повторить? [Y/N] ->");
+    isContinue = (Console.ReadLine().ToUpper() == "Y");
+    Console.WriteLine();
+} while (isContinue);
