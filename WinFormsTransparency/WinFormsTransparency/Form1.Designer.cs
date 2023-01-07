6eb9f0d82c77e8dae56856663ccef4dd693f4b99
Ksenia <ksuha280502@gmail.com>
2023-01-07 20:27:06 +0300

    Приложение в форме картинки (перемещение с помощью правой кнопки мыши)

---
+++ WinFormsTransparency/WinFormsTransparency/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WinFormsTransparency
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buClose
+            // 
+            this.buClose.Location = new System.Drawing.Point(256, 107);
+            this.buClose.Name = "buClose";
+            this.buClose.Size = new System.Drawing.Size(94, 29);
+            this.buClose.TabIndex = 0;
+            this.buClose.Text = "Close";
+            this.buClose.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WinFormsTransparency.Properties.Resources.BS;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(639, 569);
+            this.Controls.Add(this.buClose);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button buClose;
+    }
+}
