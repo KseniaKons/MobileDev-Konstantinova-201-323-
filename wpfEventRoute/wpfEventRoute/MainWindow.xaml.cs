b283b08fce83c39a08225001615cf7506f58e4ab
Ksenia <ksuha280502@gmail.com>
2023-01-09 21:16:13 +0300

    просмотр маршрутов (sender, e)

---
+++ wpfEventRoute/wpfEventRoute/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpfEventRoute
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClear(object sender, RoutedEventArgs e)
+        {
+           lbLogs.Items.Clear();
+        }
+
+        private void All_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            lbLogs.Items.Add($"All_MouseDown \n sender = {sender} \n e.Source = {e.Source}");
+        }
+
+        private void All_MouseEnter(object sender, MouseEventArgs e)
+        {
+            lbLogs.Items.Add($"{nameof(All_MouseEnter)} \n sender = {sender} \n e.Source = {e.Source}");
+        }
+    }
+}
