1b45bebd217852177cf380b25214b8aeed0c0428
Ksenia <ksuha280502@gmail.com>
2023-01-12 12:43:24 +0300

    Экзамен. Line98

--- Exam_18 (wfa)/Exam_18 (wfa)/Form1.Designer.cs
+++ Exam_18 (wfa)/Exam_18 (wfa)/Form1.Designer.cs
@@ -29,11 +29,70 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.laName = new System.Windows.Forms.Label();
+            this.laTimer = new System.Windows.Forms.Label();
+            this.cBLevel = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // laName
+            // 
+            this.laName.AutoSize = true;
+            this.laName.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.laName.Location = new System.Drawing.Point(431, 23);
+            this.laName.Name = "laName";
+            this.laName.Size = new System.Drawing.Size(130, 54);
+            this.laName.TabIndex = 1;
+            this.laName.Text = "label1";
+            // 
+            // laTimer
+            // 
+            this.laTimer.AutoSize = true;
+            this.laTimer.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.laTimer.Location = new System.Drawing.Point(880, 19);
+            this.laTimer.Name = "laTimer";
+            this.laTimer.Size = new System.Drawing.Size(65, 28);
+            this.laTimer.TabIndex = 2;
+            this.laTimer.Text = "label2";
+            // 
+            // cBLevel
+            // 
+            this.cBLevel.FormattingEnabled = true;
+            this.cBLevel.Location = new System.Drawing.Point(37, 19);
+            this.cBLevel.Name = "cBLevel";
+            this.cBLevel.Size = new System.Drawing.Size(170, 28);
+            this.cBLevel.TabIndex = 3;
+            this.cBLevel.Text = "Уровень сложности";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(3, 67);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1478, 530);
+            this.panel1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1482, 653);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.cBLevel);
+            this.Controls.Add(this.laTimer);
+            this.Controls.Add(this.laName);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private Label laName;
+        private Label laTimer;
+        private ComboBox cBLevel;
+        private System.Windows.Forms.Timer timer1;
+        private Panel panel1;
     }
 }
--- Exam_18 (wfa)/Exam_18 (wfa)/Form1.cs
+++ Exam_18 (wfa)/Exam_18 (wfa)/Form1.cs
@@ -1,10 +1,303 @@
+using System.Diagnostics;
+using System.Drawing;
+
 namespace Exam_18__wfa_
 {
     public partial class Form1 : Form
     {
+
+        string[] levels;
+        int gameLevel;
+        private PictureBox[,] piB;
+        int[,] busy = new int[100, 100];
+        private readonly Stopwatch stopwatch = new Stopwatch();
+        int kolCells;
+        int circle;
+        int colorV;
+        bool vzato = false;
+        int str, colon;
+        int strP, colonP;
+        Random rnd = new Random();
         public Form1()
         {
             InitializeComponent();
+
+            //this.cBLevel.FormattingEnabled = true;
+            this.cBLevel.Items.AddRange(new object[] {
+            "Уровень1",
+            "Уровень2",
+            "Уровень3"});
+
+            laTimer.Text = $"Прошло Времени: 00:00";
+
+            cBLevel.SelectionChangeCommitted += CBLevel_SelectionChangeCommitted;
+
+
+            laName.Text = $"LINE 98";
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    busy[i, j] = 0;
+                }
+            }
+
+            
+        }
+
+        private void CBLevel_SelectionChangeCommitted(object? sender, EventArgs e)
+        {
+            panel1.Controls.Clear();
+            timer1.Start();
+            timer1.Tick += Timer1_Tick;
+            stopwatch.Start();
+            CreateCells(gameLevel);
+            CirclePaintOne();
+            
+            Invalidate();
+        }
+
+        //private void CirclePost()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        private void Timer1_Tick(object? sender, EventArgs e)
+        {
+            
+            laTimer.Text = $"Прошло Времени: { stopwatch.Elapsed.ToString("mm\\:ss")}";
+        }
+
+        
+
+        public void chekLevel()
+        {
+           
+            levels = new string[] { "Уровень 1", "Уровень 2", "Уровень 3" };
+
+            string chooseLevel1 = cBLevel.SelectedIndex.ToString();
+
+            if (chooseLevel1 == "0")
+            {
+                gameLevel = 1;
+            }
+            else if (chooseLevel1 == "1")
+            {
+                gameLevel = 2;
+            }
+            else if (chooseLevel1 == "2")
+            {
+                gameLevel = 3;
+            }
+
+
+
+            if (gameLevel == 1)
+                kolCells = 5;
+            else if (gameLevel == 2)
+                kolCells = 7;
+            else if (gameLevel == 3)
+                kolCells = 10;
+        }
+
+        private void CreateCells(int gameLevel)
+        {
+            chekLevel();
+
+           
+            piB = new PictureBox[5, kolCells];
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < kolCells; j++)
+                {
+                    piB[i, j] = new PictureBox();
+                    piB[i, j].Size = new Size(100, 100);
+                    piB[i, j].BorderStyle = BorderStyle.FixedSingle;
+                    piB[i, j].Location = new Point(250 + j * 100, 10 + i * 100);
+                    panel1.Controls.Add(piB[i, j]);
+                    piB[i, j].Click += PerenosObj;
+                    piB[i, j].Click += PostObj;
+                }
+            }
+
+            Invalidate();
+
+        }
+
+        private void PostObj(object? sender, EventArgs e)
+        {
+            PictureBox pressedPictureBox = sender as PictureBox;
+            int id = pressedPictureBox.TabIndex.GetHashCode();
+            id++;
+
+            chekLevel();
+            int c = 1;
+            if (gameLevel == 1)
+                c = 5;
+            else if (gameLevel == 2)
+                c = 7;
+            else if (gameLevel == 3)
+                c = 10;
+
+
+            colonP = id % c;
+            if (colonP == 0)
+                colonP = c;
+
+            if (id > 0 && id < 6)
+                strP = 1;
+            else if (id > 5 && id < 11)
+                strP = 2;
+            else if (id > 10 && id < 16)
+                strP = 3;
+            else if (id > 15 && id < 21)
+                strP = 4;
+            else if (id > 20 && id < 26)
+                strP = 5;
+
+            colonP--;
+            strP--;
+
+            if (busy[strP, colonP] != 1 && busy[strP, colonP] != 2 && busy[strP, colonP] != 3)
+            {
+                if (vzato == true)
+                {
+                    vzato = false;
+                    busy[strP, colonP] = colorV;
+                    CirclePost(strP, colonP, colorV);
+                }
+
+
+            }
+        }
+
+        private void CirclePost(int _a, int _b, int colorV)
+        {
+            piB[_a, _b].Image = new Bitmap(100, 100);
+            var g = Graphics.FromImage(piB[_a, _b].Image);
+            switch (colorV)
+            {
+                case 1:
+                    g.DrawImage(Properties.Resources.BlueCircle, new Rectangle(0, 0, 100, 100));
+                    busy[_a, _b] = 1;
+                    break;
+                case 2:
+                    g.DrawImage(Properties.Resources.GreenCircle, new Rectangle(0, 0, 100, 100));
+                    busy[_a, _b] = 2;
+                    break;
+                case 3:
+                    g.DrawImage(Properties.Resources.RedCircle, new Rectangle(0, 0, 100, 100));
+                    busy[_a, _b] = 3;
+                    break;
+            }
+            Invalidate();
+        }
+
+        private void PerenosObj(object? sender, EventArgs e)
+        {
+            PictureBox pressedPictureBox = sender as PictureBox;
+            int id1 = pressedPictureBox.TabIndex.GetHashCode();
+            id1++;
+
+            chekLevel();
+            int c = 1;
+            if (gameLevel == 1)
+                c = 5;
+            else if (gameLevel == 2)
+                c = 7;
+            else if (gameLevel == 3)
+                c = 10;
+
+
+            colon = id1 % c;
+            if (colon == 0)
+                colon = c;
+
+            if (id1 > 0 && id1 < 6)
+                str = 1;
+            else if (id1 > 5 && id1 < 11)
+                str = 2;
+            else if (id1 > 10 && id1 < 16)
+                str = 3;
+            else if (id1 > 15 && id1 < 21)
+                str = 4;
+            else if (id1 > 20 && id1 < 26)
+                str = 5;
+
+            colon--;
+            str--;
+
+            if (busy[str, colon] == 1 || busy[str, colon] == 2 || busy[str, colon] == 3)
+            {
+                if (vzato == false)
+                {
+                    vzato = true;
+                    colorV = busy[str, colon];
+                    busy[str, colon] = 0;
+                    CircleVzato(str, colon, colorV);
+                }
+
+
+            }
+
+        }
+
+        private void CircleVzato(int _a, int _b, int colorV)
+        {
+            piB[_a, _b].Image = new Bitmap(50, 50);
+            var g = Graphics.FromImage(piB[_a, _b].Image);
+            switch (colorV)
+            {
+                case 1:
+                    g.DrawImage(Properties.Resources.BlueCircle, new Rectangle(0, 0, 50, 50));
+                    busy[_a, _b] = 1;
+                    break;
+                case 2:
+                    g.DrawImage(Properties.Resources.GreenCircle, new Rectangle(0, 0, 50, 50));
+                    busy[_a, _b] = 2;
+                    break;
+                case 3:
+                    g.DrawImage(Properties.Resources.RedCircle, new Rectangle(0, 0, 50, 50));
+                    busy[_a, _b] = 3;
+                    break;
+            }
+            Invalidate();
+        }
+
+        private void CirclePaintOne()
+        {
+            chekLevel();
+            int a = 10 + 5 * (gameLevel - 1) + 1;
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < kolCells; j++)
+                {
+                    circle = rnd.Next(1, a);
+                    piB[i, j].Image = new Bitmap(100, 100);
+                    var g = Graphics.FromImage(piB[i, j].Image);
+                    switch (circle)
+                    {
+                        case 1:
+                            g.DrawImage(Properties.Resources.BlueCircle, new Rectangle(0, 0, 100, 100));
+                            busy[i, j] = 1;
+                            break;
+                        case 2:
+                            g.DrawImage(Properties.Resources.GreenCircle, new Rectangle(0, 0, 100, 100));
+                            busy[i, j] = 2;
+                            break;
+                        case 3:
+                            g.DrawImage(Properties.Resources.RedCircle, new Rectangle(0, 0, 100, 100));
+                            busy[i, j] = 3;
+                            break;
+                    }
+
+                }
+            }
+            Invalidate();
         }
     }
 }
